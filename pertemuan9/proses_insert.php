408835d35bbe293b1303d735e64a749f4f7897f9
Salputrif <90301030+Salputrif@users.noreply.github.com>
2021-11-10 17:06:40 +0700

    Update proses_insert.php

--- pertemuan9/proses_insert.php
+++ pertemuan9/proses_insert.php
@@ -1,14 +1,14 @@
 <?php
  include("koneksi.php");
 
- $id_tu= $_POST['id_tu'];
+
  $provinsi= $_POST['provinsi'];
  $kabupaten= $_POST['kabupaten'];
  $kecamatan= $_POST['kecamatan'];
  $desa= $_POST['desa'];
  $pelabuhan= $_POST['pelabuhan'];
 
- $query ="insert into usaha values ('', '$id_tu', '$provinsi', '$kabupaten', '$kecamatan', '$desa', '$pelabuhan')";
+ $query ="insert into usaha values ('', '$provinsi', '$kabupaten', '$kecamatan', '$desa', '$pelabuhan')";
 
 
  if (mysqli_query($koneksi, $query)){
